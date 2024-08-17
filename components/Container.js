58461df1b7a8ac3138a0959b4fcc3d65d9247df1
tJonPope727 <notmemeapp@gmail.com>
2024-08-17 04:02:36 -0700

    fix container

--- components/Container.js
+++ components/Container.js
@@ -2,6 +2,7 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import appwriteSDK from "../utils";
 
 export default function MainContainer() {
+
     const authRedirect = async () => {
         try {
           await appwriteSDK.account.createOAuth2Session(
@@ -18,6 +19,7 @@ export default function MainContainer() {
           <Row>
             <Col>
               <h1>Pair program with your codebase</h1>
+              <p>Squire is your AI pair programmer: chat with your codebase, get an AI code review, and customize it to your style. Have greater impact with less toil - be the hero of your team and hire a Squire who doesn't need sleep.</p>
               <Button variant="outline-light" onClick={authRedirect} className="rounded-pill">Get Started for Free</Button>
             </Col>
           </Row>
